900692d7861befa122a7bc78feb34c9620e1dbc0
samir aoulad amar <144660881+samir20-23@users.noreply.github.com>
2024-06-13 12:46:25 +0100

    Add files via upload

---
+++ DataLocation/formTour.php
@@ -0,0 +1,103 @@
+<?php
+// $servername = "localhost";
+// $dbname = "bookm";
+// $username = "SAMIR";
+// $password = "samir123";
+// $tbname = "book";
+
+// function filter($data) {
+//     return htmlspecialchars(stripslashes(trim($data)));
+// }
+
+// try {
+//     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+   
+//     $stmt = $conn->prepare("INSERT INTO $tbname (book_name, book_email,  book_adults, book_child5_14, book_child2, book_depart, book_return, book_message) VALUES (:name, :email , :adults, :child5_14, :child2, :depart, :return, :message)");
+
+//     $stmt->bindParam(':name', $name);
+//     $stmt->bindParam(':email', $email); 
+//     $stmt->bindParam(':adults', $adults);
+//     $stmt->bindParam(':child5_14', $child5_14);
+//     $stmt->bindParam(':child2', $child2);
+//     $stmt->bindParam(':depart', $depart); 
+//     $stmt->bindParam(':message', $message);
+//     $stmt->bindParam(':return', $Return);
+
+//     $Return = filter($_POST['Return']);
+//     $name = filter($_POST['name']);
+//     $email = filter($_POST['email']); 
+//     $adults = filter($_POST['Adults']);
+//     $child5_14 = filter($_POST['Child5_14']);
+//     $child2 = filter($_POST['Child2']);
+//     $depart = filter($_POST['Depart']); 
+//     $message = filter($_POST['message']);
+
+//     $stmt->execute();
+
+//     header('Content-Type: application/json');
+//     echo json_encode(["msg" => "success"]);
+// } catch(PDOException $e) {
+//     echo 'error'.$e->getMessage();
+// }
+
+// $conn = null;
+?>
+
+
+<?php
+$servername = "localhost";
+$dbname = "bookm";
+$username = "SAMIR";
+$password = "samir123";
+$tbname = "book";
+
+function filter($data) {
+    return htmlspecialchars(stripslashes(trim($data)));
+}
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+   
+    $stmt = $conn->prepare("INSERT INTO $tbname (book_name, book_email,  book_adults, book_child5_14, book_child2, book_depart, book_return, book_message) VALUES (:name, :email , :adults, :child5_14, :child2, :depart, :return, :message)");
+
+    $stmt->bindParam(':name', $name);
+    $stmt->bindParam(':email', $email); 
+    $stmt->bindParam(':adults', $adults);
+    $stmt->bindParam(':child5_14', $child5_14);
+    $stmt->bindParam(':child2', $child2);
+    $stmt->bindParam(':depart', $depart); 
+    $stmt->bindParam(':message', $message);
+    $stmt->bindParam(':return', $Return);
+
+    $Return = filter($_POST['Return']);
+    $name = filter($_POST['name']);
+    $email = filter($_POST['email']); 
+    $adults = filter($_POST['Adults']);
+    $child5_14 = filter($_POST['Child5_14']);
+    $child2 = filter($_POST['Child2']);
+    $depart = filter($_POST['Depart']); 
+    $message = filter($_POST['message']);
+
+    $stmt->execute();
+
+  
+    // echo json_encode(["msg" => "success"]);
+
+  header("../bookFirst.html");
+
+    // $mynumber = "+2120718087106";
+    // $whatsappLink = "https://wa.me/$mynumber/?text=" . urlencode("I want to reserve in $name for $email dh");
+
+    // header("Location: $whatsappLink");
+    exit;
+
+} catch(PDOException $e) {
+    echo 'error'.$e->getMessage();
+}
+
+$conn = null;
+?>
